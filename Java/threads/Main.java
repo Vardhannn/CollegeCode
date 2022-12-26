714cf287f70cc1c9d3e438517badd7d6d4863c2d
Vardhannn <iamsrivardhann@gmail.com>
2022-12-26 19:10:07 +0530

    oops

---
+++ Java/threads/Main.java
@@ -0,0 +1,14 @@
+package threads;
+
+public class Main {
+    public void first(){
+        System.out.println("first");
+    }
+    public void second(){
+        System.out.println("second");
+    }
+
+public void third(){
+        System.out.println("third");
+        }
+        }
